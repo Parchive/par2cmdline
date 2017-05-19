e37491515a168c17f6a874d6c3028d2fcfab0a31
BlackEagle <ike.devolder@gmail.com>
2017-05-19 21:43:49 +0200

    bump 0.7.1
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ build/release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+git clean -xfd
+build/build.sh
+make check
+make distcheck
+version=$(ls -1 par2cmdline*.tar.gz | tail -n1 | sed -e 's/par2cmdline-\([0-9\.]\+\)\.tar\.gz/\1/')
+mkdir -p ../par2release
+mv par2cmdline-$version.tar.gz ../par2release
+(
+    cd ../par2release
+    gzcat par2cmdline-$version.tar.gz | bzip2 > par2cmdline-$version.tar.bz2
+)
+git clean -xfd
+build/build-win32.sh
+mv par2.exe ../par2release
+(
+    cd ../par2release
+    zip par2cmdline-$version-win-x86.zip par2.exe
+    rm par2.exe
+)
+git clean -xfd
+build/build-win64.sh
+mv par2.exe ../par2release
+(
+    cd ../par2release
+    zip par2cmdline-$version-win-x64.zip par2.exe
+    rm par2.exe
+)
+
+(
+    cd ../par2release
+    sha512sum *.tar* *.zip > checksums.sha512
+    for file in *.tar* *.zip; do
+        gpg --detach-sign $file
+    done
+)
+
+git tag -a --sign v$version
