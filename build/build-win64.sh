64203e784c3ee55a266b77c147e8c073ecd056ec
BlackEagle <ike.devolder@gmail.com>
2017-06-03 09:41:44 +0200

    build-win* use -s flag for gcc then we don't need strip anymore, #94
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- build/build-win64.sh
+++ build/build-win64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="-O3 -pipe -fstack-protector-strong"
-export CXXFLAGS="-O3 -pipe -fstack-protector-strong"
+export CFLAGS="-O3 -s -pipe -fstack-protector-strong"
+export CXXFLAGS="-O3 -s -pipe -fstack-protector-strong"
 export LDFLAGS="-static"
 
 # automake
@@ -10,5 +10,3 @@ export LDFLAGS="-static"
 ./configure --host=x86_64-w64-mingw32
 # make
 make
-# strip
-/usr/x86_64-w64-mingw32/bin/strip par2.exe
