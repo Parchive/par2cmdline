5c3cd0a6ce182e544506f830b78572759238f284
BlackEagle <ike.devolder@gmail.com>
2017-06-25 21:39:12 +0200

    bump 0.7.3
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- build/release.sh
+++ build/release.sh
@@ -7,6 +7,7 @@ build/build.sh
 make check
 make distcheck
 version=$(ls -1 par2cmdline*.tar.gz | tail -n1 | sed -e 's/par2cmdline-\([0-9\.]\+\)\.tar\.gz/\1/')
+[[ -d ../par2release ]] && rm -r ../par2release
 mkdir -p ../par2release
 mv par2cmdline-$version.tar.gz ../par2release
 (
