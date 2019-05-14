6e699cced3a6ea1b62f2e9db136c07d7e4463275
Michael Nahas <mike@nahas.com>
2019-05-14 09:50:36 -0500

    Updated copyright information with my name

--- src/par2fileformat.h
+++ src/par2fileformat.h
@@ -2,6 +2,7 @@
 //  repair tool). See http://parchive.sourceforge.net for details of PAR 2.0.
 //
 //  Copyright (c) 2003 Peter Brian Clements
+//  Copyright (c) 2019 Michael D. Nahas
 //
 //  par2cmdline is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
