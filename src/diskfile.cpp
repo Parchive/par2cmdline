858ae7cd4f382bf9620a4d7b2d04132b59042828
Yutaka Sawada <60930312+Yutaka-Sawada@users.noreply.github.com>
2023-07-28 21:54:05 +0900

    enable Recurse flag on Windows OS

--- src/diskfile.cpp
+++ src/diskfile.cpp
@@ -397,7 +397,8 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
       {
         matches->push_back(path + fd.cFileName);
       }
-      else if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+      else if ((0 != (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) &&
+               (recursive == true))
       {
         if (fd.cFileName[0] == '.') {
           continue;
@@ -407,6 +408,10 @@ std::unique_ptr< list<string> > DiskFile::FindFiles(string path, string wildcard
 	std::unique_ptr< list<string> > dirmatches(
 						 DiskFile::FindFiles(path + fd.cFileName, nwwildcard, true)
 						 );
+
+        // sort both lists before merge
+        matches->sort();
+        dirmatches->sort();
 
         matches->merge(*dirmatches);
       }
