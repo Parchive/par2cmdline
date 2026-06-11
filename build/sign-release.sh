c745c6d3fbb064becc4ece2b255c6b9c12d4757c
BlackEagle <ike.devolder@gmail.com>
2026-06-11 07:33:51 +0200

    use signingkey from git repo to sign build artifacts
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- build/sign-release.sh
+++ build/sign-release.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+set -x
+
+gpg_default_key=""
+local_gpgkey="$(grep 'signingkey' .git/config | sed -e 's/.*=\s\+//')"
+
+if [[ -n "$local_gpgkey" ]]; then
+  echo "$local_gpgkey"
+  gpg_default_key="--default-key $local_gpgkey"
+fi
+
 (
     cd ../par2release
     sha512sum *.tar* *.zip > checksums.sha512
     for file in *.tar* *.zip; do
-        gpg --detach-sign $file
+        gpg $gpg_default_key --detach-sign $file
     done
 )
