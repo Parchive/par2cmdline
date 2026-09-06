40715816d6154adf65d1eb40474b94b05fd125fa
mnightingale <9887246+mnightingale@users.noreply.github.com>
2026-09-06 21:24:14 +0100

    Qualify the fixed-width typedefs with std:: since <cstdint> only guarantees those names in namespace std

--- src/libpar2.h
+++ src/libpar2.h
@@ -25,14 +25,14 @@
 #include <string>
 #include <vector>
 
-typedef uint8_t  u8;
-typedef int8_t   i8;
-typedef uint16_t u16;
-typedef int16_t  i16;
-typedef uint32_t u32;
-typedef int32_t  i32;
-typedef uint64_t u64;
-typedef int64_t  i64;
+typedef std::uint8_t  u8;
+typedef std::int8_t   i8;
+typedef std::uint16_t u16;
+typedef std::int16_t  i16;
+typedef std::uint32_t u32;
+typedef std::int32_t  i32;
+typedef std::uint64_t u64;
+typedef std::int64_t  i64;
 
 
 typedef enum
