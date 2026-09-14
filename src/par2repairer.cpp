85b51b038489e2b056ea6147ebf6bdd3b9f7deba
mnightingale <9887246+mnightingale@users.noreply.github.com>
2026-09-14 11:17:02 +0100

    Report how many recovery blocks a set has when its packets are prepared

--- src/par2repairer.cpp
+++ src/par2repairer.cpp
@@ -784,6 +784,7 @@ Result Par2Repairer::PreparePackets(void)
     memcpy(info.setid.data(), setid.hash, sizeof(setid.hash));
     info.blocksize = blocksize;
     info.datablocks = sourceblockcount;
+    info.recoveryblocks = (u32)recoverypacketmap.size();
     info.recoverablefilecount = mainpacket->RecoverableFileCount();
     info.otherfilecount = mainpacket->TotalFileCount() - mainpacket->RecoverableFileCount();
     info.datasize = totaldatasize;
